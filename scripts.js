fb372355252aaa65d786894165b4ed01460c3f7e
joaocamargo <joaovcamargo2002@gmail.com>
2022-04-16 15:14:02 -0300

    Envio de mensagens

--- scripts.js
+++ scripts.js
@@ -2,30 +2,19 @@ let messagesData;
 let username;
 let userTo = "Todos"
 let usersTo;
-let teste = []
-
+let userFrom;
+let visibility = "Público"
+let visibilityApi = "message"
 const chat = document.querySelector(".chats")
-    
-function calltoActionBtn (){
-    const entrarBtn = document.querySelector(".entrarBtn")
-
-    if (username !== undefined){
-        entrarBtn.classList.add("actionBtn")
-    } else {
-        setInterval(calltoActionBtn,3000)
-    }
-}
-
-
 
 function validUser (){
 
-    username = document.querySelector(".username").value
-    const user = {
-        name: `${username}`
+    userFrom = document.querySelector(".username").value
+    username = {
+        name: `${userFrom}`
     }
 
-    const promisseValidUser = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants ", user);
+    const promisseValidUser = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants ", username);
 
     promisseValidUser.then(validatedUser);
     promisseValidUser.catch(errorValidUser);
@@ -34,7 +23,9 @@ function validUser (){
 function validatedUser (){
     const loginScreen = document.querySelector(".loginScr")
     loginScreen.classList.add("hidden")
+    
     consultData()
+    consultUsers()
     
 }
 
@@ -49,10 +40,13 @@ function consultData (){
 const promisseMessage = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages")
 promisseMessage.then(dataReceive)
 
-const usersPromisse = axios.get("https://mock-api.driven.com.br/api/v6/uol/participants")
-    usersPromisse.then(usersReceiveData)
 }
- consultData()
+
+function consultUsers(){
+    const usersPromisse = axios.get("https://mock-api.driven.com.br/api/v6/uol/participants")
+    usersPromisse.then(usersReceiveData)
+
+}
 
 
 function dataReceive (message) {
@@ -67,25 +61,33 @@ function usersReceiveData (usersPromisse){
     usersTo = usersPromisse.data
 
 
-printUsers(usersTo)    
+printUsers(usersTo) 
+messageTo()
+checkStatus()   
 }
 
 function sendMessage (){
-    const inputMessage = document.querySelector(".sendMessage")
 
-    
+    const inputMessage = document.querySelector(".writeMessage").value
+    const messagem = {
+        from: userFrom,
+        to: userTo,
+        text: inputMessage,
+        type: visibilityApi,
+    }
+    console.log(messagem)
+    const sendMessage = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", messagem)
+    sendMessage.then(console.log("deu bom"))
 
 }
 
 function checkStatus(){ 
 
     
-    const sendStatus = {
-        name: `${username}`
-      }
-    const status = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", sendStatus)
+  
+    const status = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", username)
     
-    status.then(setInterval(checkStatus,5000))
+    /* status.then(setInterval(checkStatus,10000)) */
 }
 
 function printMessage(messagesData){
@@ -117,7 +119,7 @@ function printMessage(messagesData){
         
         } else {
 
-            chat.innerHTML += `<div class="message">
+            chat.innerHTML += `<div class="message reservedMessage">
 
             <p class="time">(${messagesData[i].time })&#160;</p>
             <p class="user from">${messagesData[i].from } &#160; </p>
@@ -133,24 +135,23 @@ function printMessage(messagesData){
 }
 
 
-function printUsers (usersTo){
+function printUsers (){
 
     let usersBtn = document.querySelector(".specificUser")
     usersBtn.innerHTML = ""
-    
-    checkStatus()
+
 
     for (let i = 0; i < usersTo.length ; i++ ) {
 
         usersBtn.innerHTML = usersBtn.innerHTML + `
-            <button class="userBtn">
+            <button onclick="selectUserTo(this)" class="userBtn">
             <ion-icon class="ionicon" name="person-circle"></ion-icon>
-            <p>${usersTo[i].name}</p>
-            <ion-icon class="check hidden" name="checkmark"></ion-icon>
+            <p class="nameUser">${usersTo[i].name}</p>
+            <ion-icon class="check" name="checkmark"></ion-icon>
         </button>
         `
     
-    }
+    } /* setInterval(consultUsers,10000) */
 }
 
 
@@ -171,29 +172,74 @@ function sidebarShow(){
 
 }
 
-
-function sendMessage (){
-    
-  
-
-}
-
-
 function messageTo (){
 
     const lineMessageTo = document.querySelector(".messageTo")
 
 
-    lineMessageTo.innerHTML = `<p>Enviando para ${userTo} (Reservadamente)</p>`
-}
-messageTo()
-
-
-
-function deuBom(){
-
-    setInterval(checkStatus, 5000)
-
+    lineMessageTo.innerHTML = `<p>Enviando para ${userTo} (${visibility})</p>`
 }
 
+function visibilitySelect(element){
 
+    const checkSelect = document.querySelector(".visibilitySelect")
+    
+    if(checkSelect === null) {
+        element.children[2].classList.add("visibilitySelect")
+        visibility = element.children[1].innerHTML
+        messageType()
+        errorMsg()
+    } else {
+        checkSelect.classList.remove("visibilitySelect")
+        element.children[2].classList.add("visibilitySelect")
+        visibility = element.children[1].innerHTML
+        messageType()
+        errorMsg()
+    }
+}
+
+function messageType(){
+
+
+    if(visibility === "Público"){
+        visibilityApi = "message"
+        messageTo()
+    } else {
+        visibilityApi = "private_message"
+    messageTo()
+    }
+}
+
+function selectUserTo (element){
+
+    const checkSelect = document.querySelector(".checkSelect")
+
+    if(checkSelect === null) {
+        element.children[2].classList.add("checkSelect")
+        userTo = element.children[1].innerHTML
+        messageTo()
+        errorMsg()
+    } else {
+        checkSelect.classList.remove("checkSelect")
+        element.children[2].classList.add("checkSelect")
+        userTo = element.children[1].innerHTML
+        messageTo()
+        errorMsg()
+    }
+}
+
+
+function errorMsg () {
+
+
+    if(userTo === "Todos" && visibility === "Reservadamente") {
+        const checkSelect = document.querySelector(".checkSelect")
+        checkSelect.classList.remove("checkSelect")
+        const visibilitySelect = document.querySelector(".visibilitySelect")
+        visibilitySelect.classList.remove("visibilitySelect")
+        userTo = "Todos"
+        visibility = "Público"
+        alert("Escolha um usuário especifico para enviar uma mensagem reservada")
+        messageTo()
+    }
+}
