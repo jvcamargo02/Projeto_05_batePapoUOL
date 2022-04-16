a2cbebd2bd4603db0817aa8f914240822ee5687f
joaocamargo <joaovcamargo2002@gmail.com>
2022-04-16 18:24:29 -0300

    debug requisições api

--- scripts.js
+++ scripts.js
@@ -5,143 +5,153 @@ let usersTo;
 let userFrom;
 let visibility = "Público"
 let visibilityApi = "message"
+let teste;
+
 const chat = document.querySelector(".chats")
 
-function validUser (){
+function validUser() {
 
     userFrom = document.querySelector(".username").value
     username = {
-        name: `${userFrom}`
+        name: userFrom
     }
 
     const promisseValidUser = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants ", username);
 
     promisseValidUser.then(validatedUser);
     promisseValidUser.catch(errorValidUser);
+    
 }
 
-function validatedUser (){
+function validatedUser() {
     const loginScreen = document.querySelector(".loginScr")
     loginScreen.classList.add("hidden")
-    
-    consultData()
-    consultUsers()
-    
+
+
+    setInterval(consultData,3000)
+    setInterval(consultUsers, 10000)
+    setInterval(checkStatus,5000)
 }
 
-function errorValidUser(){
+function errorValidUser() {
     alert("Este username já está em uso, escolha outro")
 }
 
 
 
-function consultData (){
+function consultData() {
 
-const promisseMessage = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages")
-promisseMessage.then(dataReceive)
+    const promisseMessage = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages")
+    promisseMessage.then(dataReceive)
 
 }
 
-function consultUsers(){
+function consultUsers() {
     const usersPromisse = axios.get("https://mock-api.driven.com.br/api/v6/uol/participants")
     usersPromisse.then(usersReceiveData)
 
 }
 
 
-function dataReceive (message) {
-    
+function dataReceive(message) {
+
     messagesData = message.data
 
-printMessage(messagesData)
+    printMessage(messagesData)
 }
 
-function usersReceiveData (usersPromisse){
+function usersReceiveData(usersPromisse) {
 
     usersTo = usersPromisse.data
 
 
-printUsers(usersTo) 
-messageTo()
-checkStatus()   
+    printUsers(usersTo)
+    messageTo()
+
 }
 
-function sendMessage (){
+function sendMessage() {
 
-    const inputMessage = document.querySelector(".writeMessage").value
+    let inputMessage = document.querySelector(".writeMessage")
     const messagem = {
         from: userFrom,
         to: userTo,
-        text: inputMessage,
+        text: inputMessage.value,
         type: visibilityApi,
     }
     console.log(messagem)
     const sendMessage = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", messagem)
-    sendMessage.then(console.log("deu bom"))
-
+    sendMessage.then(consultData)
+    sendMessage.catch(error => console.log(error))
+    inputMessage.value = ""
 }
 
-function checkStatus(){ 
+function checkStatus() {
 
-    
-  
+
+    console.log(username)
     const status = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", username)
-    
-    /* status.then(setInterval(checkStatus,10000)) */
+
 }
 
-function printMessage(messagesData){
-    
-    
+
+function validandoConexão (){
+ 
+    setInterval(checkStatus,5000)
+}
+
+function printMessage(messagesData) {
+
+
     chat.innerHTML = "";
 
-    for (let i = 0; i < messagesData.length ; i++ ) {
-        if(messagesData[i].type === "status"){
+    for (let i = 0; i < messagesData.length; i++) {
+        if (messagesData[i].type === "status") {
             chat.innerHTML += `<div class="message statusMessage">
 
-            <p class="time">(${messagesData[i].time })&#160;</p>
-            <p class="user from">${messagesData[i].from } &#160; </p>
+            <p class="time">(${messagesData[i].time})&#160;</p>
+            <p class="user from">${messagesData[i].from} &#160; </p>
             <p class="text">${messagesData[i].text}</p>
 
         </div><!-- chat -->  `
-        
-        } else if(messagesData[i].type === "message" && (messagesData[i].to === "Todos" || messagesData[i].to === "todos" )){
+
+        } else if (messagesData[i].type === "message" && (messagesData[i].to === "Todos" || messagesData[i].to === "todos")) {
 
             chat.innerHTML += `<div class="message">
 
-            <p class="time">(${messagesData[i].time })&#160;</p>
-            <p class="user from">${messagesData[i].from } &#160; </p>
+            <p class="time">(${messagesData[i].time})&#160;</p>
+            <p class="user from">${messagesData[i].from} &#160; </p>
             <p class="user" >para &#160; </p>
-            <p class="user to">${messagesData[i].to } &#160; </p>
+            <p class="user to">${messagesData[i].to} &#160; </p>
             <p class="text">${messagesData[i].text}</p>
 
         </div><!-- chat --> `
-        
-        } else {
+
+        } else if (messagesData[i].to === userFrom || messagesData[i].from === userFrom) {
 
             chat.innerHTML += `<div class="message reservedMessage">
 
-            <p class="time">(${messagesData[i].time })&#160;</p>
-            <p class="user from">${messagesData[i].from } &#160; </p>
+            <p class="time">(${messagesData[i].time})&#160;</p>
+            <p class="user from">${messagesData[i].from} &#160; </p>
             <p class="user" >para &#160; </p>
-            <p class="user to">${messagesData[i].to } &#160; </p>
+            <p class="user to">${messagesData[i].to} &#160; </p>
             <p class="text">${messagesData[i].text}</p>
 
         </div><!-- chat --> `
-        
-        } 
-    } 
-    /* setInterval(consultData,3000) */
+
+        }
+    }
+
 }
 
 
-function printUsers (){
+function printUsers() {
 
     let usersBtn = document.querySelector(".specificUser")
     usersBtn.innerHTML = ""
 
 
-    for (let i = 0; i < usersTo.length ; i++ ) {
+    for (let i = 0; i < usersTo.length; i++) {
 
         usersBtn.innerHTML = usersBtn.innerHTML + `
             <button onclick="selectUserTo(this)" class="userBtn">
@@ -150,21 +160,21 @@ function printUsers (){
             <ion-icon class="check" name="checkmark"></ion-icon>
         </button>
         `
-    
-    } /* setInterval(consultUsers,10000) */
+
+    } 
 }
 
 
-function quitSidebar (){
+function quitSidebar() {
 
     const sidebar = document.querySelector(".sidebar")
 
     sidebar.classList.add("hidden")
-  
+
 }
 
 
-function sidebarShow(){
+function sidebarShow() {
 
     const sidebar = document.querySelector(".sidebar")
 
@@ -172,7 +182,7 @@ function sidebarShow(){
 
 }
 
-function messageTo (){
+function messageTo() {
 
     const lineMessageTo = document.querySelector(".messageTo")
 
@@ -180,11 +190,11 @@ function messageTo (){
     lineMessageTo.innerHTML = `<p>Enviando para ${userTo} (${visibility})</p>`
 }
 
-function visibilitySelect(element){
+function visibilitySelect(element) {
 
     const checkSelect = document.querySelector(".visibilitySelect")
-    
-    if(checkSelect === null) {
+
+    if (checkSelect === null) {
         element.children[2].classList.add("visibilitySelect")
         visibility = element.children[1].innerHTML
         messageType()
@@ -198,23 +208,23 @@ function visibilitySelect(element){
     }
 }
 
-function messageType(){
+function messageType() {
 
 
-    if(visibility === "Público"){
+    if (visibility === "Público") {
         visibilityApi = "message"
         messageTo()
     } else {
         visibilityApi = "private_message"
-    messageTo()
+        messageTo()
     }
 }
 
-function selectUserTo (element){
+function selectUserTo(element) {
 
     const checkSelect = document.querySelector(".checkSelect")
 
-    if(checkSelect === null) {
+    if (checkSelect === null) {
         element.children[2].classList.add("checkSelect")
         userTo = element.children[1].innerHTML
         messageTo()
@@ -229,10 +239,10 @@ function selectUserTo (element){
 }
 
 
-function errorMsg () {
+function errorMsg() {
 
 
-    if(userTo === "Todos" && visibility === "Reservadamente") {
+    if (userTo === "Todos" && visibility === "Reservadamente") {
         const checkSelect = document.querySelector(".checkSelect")
         checkSelect.classList.remove("checkSelect")
         const visibilitySelect = document.querySelector(".visibilitySelect")
@@ -243,3 +253,4 @@ function errorMsg () {
         messageTo()
     }
 }
+
